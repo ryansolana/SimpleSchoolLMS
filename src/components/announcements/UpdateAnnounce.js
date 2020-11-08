c89f1a15f761a5eea9c6fc7f66edee61f07c69fc
Ryan S <pyrowh@gmail.com>
2020-11-08 17:19:26 -0500

    fixed updateannounce with manual db call with firebase

--- src/components/announcements/UpdateAnnounce.js
+++ src/components/announcements/UpdateAnnounce.js
@@ -4,9 +4,7 @@ import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 import { updateAnnounce } from '../../store/actions/announceActions'
 import { Redirect } from 'react-router-dom'
-import { Editor } from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import { EditorState, ContentState, convertToRaw, convertFromRaw} from 'draft-js';
+import * as firebase from 'firebase'
 
 class UpdateAnnounce extends Component {
 
@@ -17,20 +15,41 @@ class UpdateAnnounce extends Component {
             // firebase auth included as per mapStateToProps
             title: '',
             content: '',
-            //editorState: EditorState.createEmpty(),
             contentLink: ''
         }
     }
 
     componentDidMount(){
-        console.log("cdm")
-        const { auth, announce } = this.props;
+        var db = firebase.firestore()
 
-        if (announce){
-            this.setState({title: announce.title}, () => console.log("title rerender: " + this.state));
-            console.log("cdm2")
-        }   
+        console.log("id: " + this.props.match.params.id)
+        console.log("cdm")
+
+        // get data first
+        var docRef = db.collection("announces").doc(this.props.match.params.id);
+        var data;
+
+        docRef.get().then(function(doc) {
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+                data = doc.data();
+                // reload when found with new state
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
+        }).then(() =>{
+            this.setState({title: data.title, content: data.content, contentLink: data.contentLink})
+        })
+
+        
+
+        console.log("state is now: ")
+        console.log(this.state)
     }
+    
     handleChange = (e) => {
         this.setState({
             [e.target.id]: e.target.value
@@ -51,40 +70,39 @@ class UpdateAnnounce extends Component {
     }
 
     render(){
+        const { auth } = this.props;
+
         console.log("component rendered")
-        const { auth, announce } = this.props;
 
         if (!auth.uid) return <Redirect to='/signin' /> // redirect to signin if user is not logged in
 
-        if (announce){
-            return (
-                <div className="container z-depth-1"> 
-                    <form onSubmit={this.handleSubmit} className="white">
-                    <h5 className="grey-text text-darken-3">Update Announcement</h5>
-                        <div className="input-field">
-                            <label className="active" htmlFor="email">Announcement Title</label>
-                            <input type="text" id="title" onChange={this.handleChange} defaultValue={announce.title} required/>
-                        </div>
-    
-                        <div className="input-field">
-                        <label htmlFor="content">Announcement Content (400 words max)</label>
-                        <textarea id="content" className="materialize-textarea" onChange={this.handleChange} max="400" rows="2" cols="200" defaultValue={announce.content} required></textarea>
-                        </div>
-
-                        <div className="input-field">
-                            <label className="active" htmlFor="email">Link URL Address (optional)</label>
-                            <input type="text" id="contentLink" onChange={this.handleChange} defaultValue={announce.contentLink}/>
-                        </div>
-    
-                        <div className="input-field">
-                            <button className="btn pink lighten-1 z-depth-0 hoverable">Update Announcement</button>
-                        </div>
-                    </form>
-                </div>
-            )
-        } else{
-            return(<div class="progress">
+        if (this.state === null){
+            return (<div class="progress">
             <div class="indeterminate"></div>
+        </div>)
+        } else {
+            return(<div className="container z-depth-1"> 
+            <form onSubmit={this.handleSubmit} className="white">
+            <h5 className="grey-text text-darken-3">Update Announcement</h5>
+                <div className="input-field">
+                    <label className="active" htmlFor="email">Announcement Title</label>
+                    <input type="text" id="title" onChange={this.handleChange} defaultValue={this.state.title} required/>
+                </div>
+
+                <div className="input-field">
+                <label htmlFor="content">Announcement Content (400 words max)</label>
+                <textarea id="content" className="materialize-textarea" onChange={this.handleChange} max="400" rows="2" cols="200" defaultValue={this.state.content} required></textarea>
+                </div>
+
+                <div className="input-field">
+                    <label className="active" htmlFor="email">Link URL Address (optional)</label>
+                    <input type="text" id="contentLink" onChange={this.handleChange} defaultValue={this.state.contentLink}/>
+                </div>
+
+                <div className="input-field">
+                    <button className="btn pink lighten-1 z-depth-0 hoverable">Update Announcement</button>
+                </div>
+            </form>
         </div>)
         }
     } 
