e80a175098f00cc471636144d7b242d6d82cd603
Pyrowh <pyrowh@gmail.com>
2020-11-25 11:44:35 -0500

    implemented forgotpassword page and component

--- src/components/auth/ForgotPassword.js
+++ src/components/auth/ForgotPassword.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import * as firebase from 'firebase'
 
 class ForgotPassword extends Component {
     state = { 
         email: '',
-        password: ''
+        password: '',
+        emailSentError: false,
+        emailSent: false
     }
 
     handleChange = (e) => {
@@ -16,28 +18,50 @@ class ForgotPassword extends Component {
     }
 
     handleSubmit = (e) => {
-        e.preventDefault();
+        var auth = firebase.auth();
+
+        if (!this.state.emailSent){
+            e.preventDefault();
+            auth.sendPasswordResetEmail(this.state.email).then(() =>{
+                this.setState({emailSent: true})
+            }).catch(function(error) {
+                this.setState({emailSentError: true})
+            });
+        }
     }
 
     render(){
-        const { authError, auth } = this.props;
+        const { auth } = this.props;
         if (auth.uid) return <Redirect to='/' /> // redirect to dashboard if user is not logged in
         return (
             <div className="container login-page">
                 <form onSubmit={this.handleSubmit} className="white">
                     <div className="card z-depth-4">
                         <div className="card-content">
-                            <h5 className="grey-text text-darken-3 center" style={{float: 'center'}}>Forgot your password?</h5>
-                            <p className="grey-text text-darken-3 center" style={{float: 'center'}}>Please enter your email address so we can help reset your password.</p>
+                            {!this.state.emailSent ? 
+                                <div>
+                                    <h5 className="grey-text text-darken-3 center" style={{float: 'center'}}>Forgot your password?</h5>
+                                    <p className="grey-text text-darken-3 center" style={{float: 'center'}}>Please enter your email address so we can help reset your password.</p>
+                                </div> : 
+                                <div>
+                                    <h5 className="grey-text text-darken-3 center" style={{float: 'center'}}>Please check your email!</h5>
+                                    <p className="grey-text text-darken-3 center" style={{float: 'center'}}>We've sent you an email containing information on how to reset your password.</p>
+                                </div>
+                            }
                         </div>
                         <div className="card-action">
                             <div className="input-field">
-                                <label htmlFor="email">EMAIL</label>
-                                <input type="email" id="email" maxLength="50" size="50" onChange={this.handleChange}/>
+                                {!this.state.emailSent && <div>
+                                    <label htmlFor="email">EMAIL</label>
+                                    <input type="email" id="email" maxLength="100" onChange={this.handleChange}/>
+                                </div>
+                                }
                             </div>
-                            <div className="input-field center">
-                                { authError ? <p className="red-text">{authError && "Failed to authenticate"}</p> : null }                       
-                                <button className="btn lighten-1 z-depth-0 login-btn center">Reset Password</button>
+                            <div className="input-field center">                  
+                                {
+                                    !this.state.emailSent && this.state.email.length > 1 ? <button className="btn lighten-1 z-depth-0 login-btn center">Reset Password</button> :
+                                    <button className="btn lighten-1 z-depth-0 login-btn center disabled">Reset Password</button>
+                                }
                             </div>
                         </div>
                     </div>
@@ -50,7 +74,6 @@ class ForgotPassword extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        authError: state.auth.authError,
         auth: state.firebase.auth
     }
 }
